cd635d63a5c8c5d00b9d4d50f5b5559b2e5196e4
sehyunyuk <sdfg8931@ajou.ac.kr>
2024-12-10 05:03:51 +0900

    add matrix calculation

--- 2d-image/q_model.py
+++ 2d-image/q_model.py
@@ -44,7 +44,6 @@ class QModel(nn.Module):
             for i in range(self.n_wires):
                 qml.RY(theta[layer_count * self.n_wires + i * 2], wires=i)
 
-
         return [qml.expval(qml.PauliZ(i)) for i in range(3)]
 
     def forward(self, x):
